d8c2107d2f5d20841822a8dc87b31240945edac0
Nurikhsan <ikhsanhikari29@gmail.com>
2019-02-26 17:40:57 +0700

    Init

---
+++ Mustache/src/main/java/com/mustache/example/dao/EmployeeDAO.java
@@ -0,0 +1,28 @@
+package com.mustache.example.dao;
+
+import com.mustache.example.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class EmployeeDAO {
+
+    public List<Employee> getEmployees()  {
+
+        Date hireDate1= Date.valueOf(LocalDate.parse("2000-12-11"));
+        Employee e1= new Employee(1L, "E01", "Tom", hireDate1);
+
+        Date hireDate2= Date.valueOf(LocalDate.parse("2001-12-21"));
+        Employee e2= new Employee(2L, "E02", "Jerry", hireDate2);
+
+        List<Employee> list= new ArrayList<Employee>();
+        list.add(e1);
+        list.add(e2);
+        return list;
+    }
+
+}
